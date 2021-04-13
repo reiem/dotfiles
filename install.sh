2f3382c03b77815ade80a9ace1f29b4c50cc4d77
Stefan Meier <slm@meiersolutions.dk>
2021-04-13 23:34:15 +0200

    renamed main to master

---
+++ install.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+DOTFILES="$(pwd)"
+COLOR_GRAY="\033[1;38;5;243m"
+COLOR_BLUE="\033[1;34m"
+COLOR_GREEN="\033[1;32m"
+COLOR_RED="\033[1;31m"
+COLOR_PURPLE="\033[1;35m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_NONE="\033[0m"
+
+title() {
+    echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
+    echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
+}
+
+error() {
+    echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
+    exit 1
+}
+
+warning() {
+    echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
+}
+
+info() {
+    echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
+}
+
+success() {
+    echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
+}
+
+get_linkables() {
+    find -H "$DOTFILES" -maxdepth 3 -name '*.symlink'
+}
+
+setup_symlinks() {
+    title "Creating symlinks"
+
+    if [ ! -e "$HOME/.dotfiles" ]; then
+        info "Adding symlink to dotfiles at $HOME/.dotfiles"
+        ln -s "$DOTFILES" ~/.dotfiles
+    fi
+
+    for file in $(get_linkables) ; do
+        target="$HOME/.$(basename "$file" '.symlink')"
+        if [ -e "$target" ]; then
+            info "~${target#$HOME} already exists... Skipping."
+        else
+            info "Creating symlink for $file"
+            ln -s "$file" "$target"
+        fi
+    done
+
+    echo -e
+    info "installing to ~/.config"
+    if [ ! -d "$HOME/.config" ]; then
+        info "Creating ~/.config"
+        mkdir -p "$HOME/.config"
+    fi
+
+    config_files=$(find "$DOTFILES/config" -maxdepth 1 2>/dev/null | grep -v -e "$DOTFILES/config$")
+    for config in $config_files; do
+        target="$HOME/.config/$(basename "$config")"
+        if [ -e "$target" ]; then
+            info "~${target#$HOME} already exists... Skipping."
+        else
+            info "Creating symlink for $config"
+            ln -s "$config" "$target"
+        fi
+    done
+}
+
+case "$1" in
+    link)
+        setup_symlinks
+        ;;
+    all)
+        setup_symlinks
+        ;;
+    *)
+        echo -e $"\nUsage: $(basename "$0") {link|all}\n"
+        exit 1
+        ;;
+esac
+
+echo -e
+success "Done."
