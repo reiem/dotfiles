e57f1a075f638ec7b2f271f9cc4d7e48ea8770a6
Stefan Meier <stefan.meier@consultant.lego.com>
2023-05-14 20:27:20 +0200

    treesitter added to nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,6 +21,13 @@ require("lazy").setup({
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
-	{ "ellisonleao/gruvbox.nvim", priority = 1000 }
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ':TSUpdate'
+	}
 })
 
